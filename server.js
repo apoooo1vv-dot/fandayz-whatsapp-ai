8d9e79ece90313f35a58ead03016a012d9d9e47f
apoooo1vv-dot <apoooo1.v.v@gmail.com>
2026-05-27 07:39:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("WhatsApp AI Bot is running");
+  res.send("WhatsApp AI Bot Running");
 });
 
 const PORT = process.env.PORT || 3000;
